94ac3a1525aad5db8ee3a74318a50f5c4c961d12
GuRgEnA <agurg21@freeuni.edu.ge>
2024-01-15 23:23:32 +0400

    Test wallets in memory

---
+++ tests/in_memory/test_wallets_in_memory.py
@@ -0,0 +1,99 @@
+from unittest.mock import ANY
+from uuid import uuid4
+
+import pytest
+
+from core.errors import DoesNotExistError, WalletsLimitError, InvalidApiKeyError, \
+    WalletPermissionError
+from core.user import generate_api_key
+from infra.constants import STARTING_BITCOIN_AMOUNT, WALLETS_LIMIT
+from infra.in_memory.users import UsersInMemory
+from infra.in_memory.wallets import WalletsInMemory
+
+
+def test_create_wallet_in_memory() -> None:
+    users = UsersInMemory()
+    user = users.create()
+
+    wallets = WalletsInMemory(users)
+    wallet = wallets.create(user.api_key)
+
+    assert wallet.user_id == user.id
+    assert wallet.address == ANY
+    assert wallet.balance == STARTING_BITCOIN_AMOUNT
+
+
+def test_create_wallet_reach_limit_in_memory() -> None:
+    users = UsersInMemory()
+    user = users.create()
+
+    wallets = WalletsInMemory(users)
+    for i in range(WALLETS_LIMIT):
+        wallets.create(user.api_key)
+
+    with pytest.raises(WalletsLimitError):
+        wallets.create(user.api_key)
+
+
+def test_create_wallet_with_unknown_key_in_memory() -> None:
+    wallets = WalletsInMemory(UsersInMemory())
+
+    with pytest.raises(InvalidApiKeyError):
+        wallets.create(generate_api_key())
+
+
+def test_read_wallet_in_memory() -> None:
+    users = UsersInMemory()
+    user = users.create()
+
+    wallets = WalletsInMemory(users)
+    wallet = wallets.create(user.api_key)
+
+    result_wallet = wallets.read(wallet.address, user.api_key)
+
+    assert result_wallet == wallet
+
+
+def test_read_wallet_with_wrong_key_in_memory() -> None:
+    users = UsersInMemory()
+    user = users.create()
+
+    wallets = WalletsInMemory(users)
+    wallet = wallets.create(user.api_key)
+
+    with pytest.raises(InvalidApiKeyError):
+        wallets.read(wallet.address, generate_api_key())
+
+
+def test_read_unknown_wallet_in_memory() -> None:
+    users = UsersInMemory()
+    user = users.create()
+
+    wallets = WalletsInMemory(users)
+
+    with pytest.raises(DoesNotExistError):
+        wallets.read(uuid4(), user.api_key)
+
+
+def test_read_others_wallet_in_memory() -> None:
+    users = UsersInMemory()
+    user1 = users.create()
+    user2 = users.create()
+
+    wallets = WalletsInMemory(users)
+    wallet = wallets.create(user1.api_key)
+
+    with pytest.raises(WalletPermissionError):
+        wallets.read(wallet.address, user2.api_key)
+
+
+def test_read_wallet_ignore_permission_in_memory() -> None:
+    users = UsersInMemory()
+    user1 = users.create()
+    user2 = users.create()
+
+    wallets = WalletsInMemory(users)
+    wallet = wallets.create(user1.api_key)
+
+    result_wallet = wallets.read(wallet.address, user2.api_key, False)
+    assert result_wallet == wallet
