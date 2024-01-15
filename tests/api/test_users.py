41a40e45f46b164fed6d7602ef61993e141633bf
GuRgEnA <agurg21@freeuni.edu.ge>
2024-01-15 22:58:16 +0400

    Test users api

---
+++ tests/api/test_users.py
@@ -0,0 +1,18 @@
+from unittest.mock import ANY
+
+import pytest
+from fastapi.testclient import TestClient
+
+from runner.setup import init_app
+
+
+@pytest.fixture
+def client() -> TestClient:
+    return TestClient(init_app())
+
+
+def test_should_create_user(client: TestClient) -> None:
+    response = client.post("/users")
+
+    assert response.status_code == 201
+    assert response.json() == {"user": {"id": ANY, "api_key": ANY}}
